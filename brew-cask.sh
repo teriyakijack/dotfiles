d76b5d39aeab08d26cb50ce2bc5e0ffe2c853709
Macintosh <mac@MacBook-Pro.local>
2018-03-17 03:49:16 +0700

    initial setup

---
+++ brew-cask.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+
+# dev
+brew cask install phpstorm
+brew cask install visual-studio-code
+brew cask install google-chrome
+brew cask install gitup
+brew cask install mysqlworkbench
+brew cask install rocket-chat
+brew cask install postman
+brew cask install java
+brew cask install android-studio
+
+# fun
+brew cask install spotify
+brew cask install spotify-notifications
+brew cask install iina
+brew cask install transmission
+
+
+brew tap caskroom/fonts 
+brew cask install font-fira-code
---
+++ brew.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install homebrew/php/php56 --with-gmp
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+brew install git
+brew install git-lfs
+brew install ssh-copy-id
+brew install tree
+
+brew install httpd
+brew install mysql
+brew install php56
+brew install php56-amqp
+brew install php56-apcu
+brew install php56-igbinary
+brew install php56-mcrypt
+brew install php56-opcache
+brew install php56-redis
+brew install php56-xdebug
+brew install php72
+brew install php72-apcu
+brew install php72-igbinary
+brew install php72-opcache
+brew install php72-redis
+brew install php72-xdebug
+brew install composer
+brew install nvm
+
+# Remove outdated versions from the cellar.
+brew cleanup
